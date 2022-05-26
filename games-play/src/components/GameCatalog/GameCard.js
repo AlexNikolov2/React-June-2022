9bf5b222e6ce7d32a694b3cacfcb871a5af6f5a3
Aleks Nikolov <tcr.official.world@gmail.com>
2022-05-27 00:10:16 +0300

    the communist system, we work on it

---
+++ games-play/src/components/GameCatalog/GameCard.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const GameCard = ({
+    game,
+}) => {
+    return (
+        <div className="allGames">
+            <div className="allGames-info">
+                <img src={game.imageUrl} alt="kzl"/>
+                <h6>{game.category}</h6>
+                <h2>{game.title}</h2>
+                {/* <p>Rating: {ratingCalculator()}</p> */}
+                <Link to={`/games/${game._id}`} className="details-button">Details</Link>
+            </div>
+        </div>
+    );
+}
+
+export default GameCard;
