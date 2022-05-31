f8ded8c2170fbffffdaaaabb30350a00f99d3feb
Aleks Nikolov <tcr.official.world@gmail.com>
2022-05-31 22:06:08 +0300

    controller done

--- memories/server/controllers/postController.js
+++ memories/server/controllers/postController.js
@@ -13,3 +13,72 @@ export const getPosts = async (req, res) => {
     }
 };
 
+export const getPost = async (req, res) => {
+    try {
+        const post = await PostMessage.findById(req.params.id);
+        res.status(200).json(post);
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+}
+
+export const createPost = async (req, res) => {
+    const {title, message, selectedFile, creator, tags} = req.body;
+    const newPostMessage = new PostMessage({
+        title,
+        message,
+        selectedFile,
+        creator,
+        tags
+    });
+    try {
+        await newPostMessage.save();
+        res.status(201).json(newPostMessage);
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+}
+
+export const updatePost = async (req, res) => {
+    const { id } = req.params;
+    const {title, message, selectedFile, creator, tags} = req.body;
+
+    if(!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send({message: 'Invalid ID'});
+    const updatedPostMessage = {
+        title,
+        message,
+        selectedFile,
+        creator,
+        tags
+    };
+    try {
+        const post = await PostMessage.findByIdAndUpdate(id, updatedPostMessage, {new: true});
+        res.status(200).json(post);
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+}
+
+export const deletePost = async (req, res) => {
+    const { id } = req.params;
+    if(!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send({message: 'Invalid ID'});
+    try {
+        const post = await PostMessage.findByIdAndRemove(id);
+        res.status(200).json(post);
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+}
+
+export const likePost = async (req, res) => {
+    const { id } = req.params;
+    if(!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send({message: 'Invalid ID'});
+    try {
+        const post = await PostMessage.findById(id);
+        post.likeCount++;
+        await post.save();
+        res.status(200).json(post);
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+}
