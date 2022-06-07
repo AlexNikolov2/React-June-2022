e093fbba09f4241a61ae20c9400d518506d4d17c
Aleks Nikolov <tcr.official.world@gmail.com>
2022-06-07 18:56:30 +0300

    post router

--- memories/server/routes/posts.js
+++ memories/server/routes/posts.js
@@ -3,12 +3,12 @@ const express = require('express');
 const { getPosts, getPost, createPost, updatePost, likePost, deletePost } = require('../controllers/postController');
 
 const router = express.Router();
+const auth = require('../middlewares/auth');
 
 router.get('/', getPosts);
-router.get('/:id', getPost);
-router.post('/', createPost);
-router.patch('/:id', updatePost);
-router.patch('/:id/likePost', likePost);
-router.delete('/:id', deletePost);
+router.post('/',auth, createPost);
+router.patch('/:id', auth, updatePost);
+router.patch('/:id/likePost', auth, likePost);
+router.delete('/:id',auth, deletePost);
 
 module.exports = router;
