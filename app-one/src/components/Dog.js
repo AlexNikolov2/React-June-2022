2477df9ba56cf2c98e558849b12bdc7a29437519
Aleks Nikolov <tcr.official.world@gmail.com>
2022-06-28 20:14:49 +0300

    react tests

---
+++ app-one/src/components/Dog.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Dog(){
+    return (
+        <div>
+        <h3>Hello from Dog!</h3>
+        <p>Hello my name is Ivan from Draganovo</p>
+        <span>Here is a wonderful picture of me:</span>
+        <img src="app-one\public\img\dog.jpg" alt="unable to load img"/>
+        </div>
+    )
+}
+
+export default Dog;
