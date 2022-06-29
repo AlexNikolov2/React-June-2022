823ff7f91b53a06d54e7e714315b14ff4d59bf41
Aleks Nikolov <tcr.official.world@gmail.com>
2022-06-29 11:25:22 +0300

    testing react

--- app-one/src/components/Dog.js
+++ app-one/src/components/Dog.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import doggo from '../img/dog.jpg'
+import './Dog.css'
 
 function Dog(){
     return (
-        <div>
+        <div className="dog">
         <h3>Hello from Dog!</h3>
         <p>Hello my name is Ivan from Draganovo</p>
+        <p className="styled-text">I am a styled component thanks to normal, imported CSS.</p>
         <span>Here is a wonderful picture of me:</span>
         <br/>
         <img src={doggo} alt="unable to load img"/>
