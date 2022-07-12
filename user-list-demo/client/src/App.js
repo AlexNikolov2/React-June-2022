a4139c6e65643bad1db48c36d1236873bdb588f8
Aleks Nikolov <tcr.official.world@gmail.com>
2022-07-12 21:04:46 +0300

    fixes

--- user-list-demo/client/src/App.js
+++ user-list-demo/client/src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
+import { Header } from './components/common/Header';
+import { Footer } from './components/common/Footer';
+import { Search } from './components/search/Search';
+import { UserList } from './components/user-list/UserList';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div>
+            <Header />
+
+            <main className="main">
+                <section className="card users-container">
+                    <Search />
+
+                    <UserList />
+                </section>
+            </main>
+
+            <Footer />
+        </div>
+    );
 }
 
 export default App;
